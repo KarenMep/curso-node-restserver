02f27bde8a6d3fd6829cdc8ba5dec814525b4bfa
Karen <mepkaren@gmail.com>
2022-07-20 12:00:57 -0500

    GET: Obtener todos los usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,16 +2,23 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs')
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
 
-    const {q,nombre = 'no hay', apikey, page= 1, limit} = req.query;
+    // const {q,nombre = 'no hay', apikey, page= 1, limit} = req.query;
+
+    const {limite = 5, desde=0} = req.query;
+    const query = {estado: true}
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ])
 
     res.json({
-        ok: true,
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey
+        total,
+        usuarios
     });
 
 }
@@ -23,7 +30,6 @@ const usuariosPut =  async(req, res) => {
     const {_id, password, google, correo, ...resto } = req.body;
 
     // TODO: Validar base de datos
-
     if(password){
         // Encriptar la contraseña
         const salt = bcryptjs.genSaltSync();
@@ -32,11 +38,7 @@ const usuariosPut =  async(req, res) => {
 
     const usuario = await Usuario.findByIdAndUpdate( id, resto )
 
-    res.json({
-        ok: true,
-        msg: 'put API - usuariosPut',
-        usuario
-    });
+    res.json(usuario);
 }
 
 const usuariosPost = async(req, res) => {
